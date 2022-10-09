d98be3b66ec7c42e6fcd81e8fc2c3d15561580d2
ashish8976 <113491231+ashish8976@users.noreply.github.com>
2022-10-09 23:22:04 +0530

    Add table in pgadmin4

--- src/data/table/Stock.kt
+++ src/data/table/Stock.kt
@@ -4,5 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object Stock : Table()
 {
-
+    val Stock_id = varchar("stock_id",10)
+    val Product_id = varchar("product_id",10).references(Product.Product_id)
+    val total_stock = varchar("total stock",50)
 }
