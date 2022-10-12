18ddb8ca9c2d481f17f85a2027e60d5f878bec47
ashish8976 <113491231+ashish8976@users.noreply.github.com>
2022-10-13 01:17:39 +0530

    some Changes

--- src/data/table/AccessoriesTable.kt
+++ src/data/table/AccessoriesTable.kt
@@ -5,7 +5,6 @@ import org.jetbrains.exposed.sql.Table
 object AccessoriesTable : Table() {
     val Access_id  = varchar("access_id",10)
     val Product_id = varchar("product_id",10).references(ProductTable.Product_id)
-    val  Color_id = varchar("color_id",10).references(ColorTable.Color_id)
     // val  Price = varchar("price",)
 
 
