cd60127033b91f243a050637467bda39f7010ea4
kunal <Kunalvirendra2510@outlook.com>
2022-11-01 13:42:07 +0530

    colour incomplet

--- src/repository/Order_Repo.kt
+++ src/repository/Order_Repo.kt
@@ -1,4 +1,32 @@
 package com.example.repository
 
+import com.example.data.model.Order
+import com.example.data.table.OrderTable
+import org.jetbrains.exposed.sql.insert
+
 class Order_Repo {
-}
+    suspend fun addOrder(order: Order)
+    {
+        Database_Factory.dbQuery{
+            OrderTable.insert { at ->
+            at[OrderTable.Order_id] = order.order_id
+            at[OrderTable.Cart_id] = order.cart_id
+            at[OrderTable.Date] = order.order_date
+            at[OrderTable.Order_Status] = order.order_status
+
+            }
+        }
+    }
+
+    class OrderRepo(private val productRepo: Product_Repo) : Mobile_Repo() , Accessories_Repo() {
+
+        //add user's order
+        suspend fun addOrder(items: List<CartItem>) = dbQuery {
+
+            val orderId = "order" + System.currentTimeMillis()
+
+            var total = 0.0
+            items.forEach {
+                total += it.totalPrice
+            }
+        }
--- src/routes/Colour_Route.kt
+++ src/routes/Colour_Route.kt
@@ -1,2 +1,32 @@
 package com.example.routes
 
+import com.example.data.model.Color
+import com.example.data.response.Simple_Response
+import com.example.repository.Color_Repo
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import sun.security.ec.CurveDB
+
+@Suppress("UNREACHABLE_CODE")
+fun Route.Colour_Route(db: Color_Repo){
+
+    post("Colour/add") {
+        val c = try{
+            call.receive<Color>()
+        }catch (e:Exception){
+            call.respond(HttpStatusCode.BadRequest,Simple_Response(false,"Some Problem Occur"))
+            return@post
+        }
+
+        try {
+            db.addColor(c)
+            call.respond(HttpStatusCode.OK,Simple_Response(true,"Colour Added Successfully"))
+        }catch (e:Exception){
+            call.respond(HttpStatusCode.Conflict,Simple_Response(false,"$e Occured"))
+        }
+    }
+}
+
