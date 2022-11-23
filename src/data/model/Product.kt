2cacd9d4e473ed6c5941586e42f5878bdd58b0e3
ashish8976 <113491231+ashish8976@users.noreply.github.com>
2022-11-23 13:20:30 +0530

    Product Repo Changes

--- src/data/model/Product.kt
+++ src/data/model/Product.kt
@@ -1,13 +1,13 @@
 package com.example.data.model
 
 data class Product(
-    val product_id : Int,
-    val product_name : String,
-    val product_desc : String,
-    val cate_name : String,
-    val color : List<Color>,
-    val brand_id : Int,
-    var Mobile : List<Mobile>,
-    var Accessories : List<Accessories>
+    val product_id: Int,
+    val product_name: String,
+    val product_desc: String,
+    val cate_name: String,
+    val color: List<Color>,
+    val brand_id: Int,
+    var Mobile: List<Mobile>? = null,
+    var Accessories: List<Accessories>? = null
 
 )
