2b3ade1fec94bc161975a7c6ad2acf08ff8d53f7
kunal <Kunalvirendra2510@outlook.com>
2022-11-28 15:07:46 +0530

    cart change

--- src/routes/Cart_Route.kt
+++ src/routes/Cart_Route.kt
@@ -1,6 +1,7 @@
 package com.example.routes
 
-import com.example.data.model.Cart
+
+import com.example.data.model.Product
 import com.example.data.response.Simple_Response
 import com.example.repository.Cart_Repo
 import io.ktor.application.*
@@ -13,26 +14,32 @@ import io.ktor.routing.*
 fun Route.Cart_Route(db: Cart_Repo){
 
     post("cart/add") {
-        val addCart = try {
-            call.receive<Cart>()
-        }
-        catch (e:Exception)
-        {
-            call.respond(HttpStatusCode.BadRequest,Simple_Response(false,"${e.message}"))
+        val email = try {
+            call.request.queryParameters["email"]
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.BadRequest, Simple_Response(false, "Hii ${e.message}"))
             return@post
         }
 
-        try
-        {
-            val cart = Cart(addCart.cart_id,addCart.email,
-                addCart.product_id,
-                addCart.quentity,addCart.total_price)
-//              db.addtocart()
-            call.respond(HttpStatusCode.Conflict,Simple_Response(true,"Cart added successfully"))
+        val product = try {
+            call.receive<Product>()
+        }catch (e:Exception){
+            call.respond(HttpStatusCode.BadRequest,Simple_Response(false,"Invaild Product"))
         }
-        catch (e : Exception){
-            call.respond(HttpStatusCode.Conflict,Simple_Response(false,"${e.message}"))
+        val qty = try{
+            call.request.queryParameters["qty"]?.toInt()
+        }catch (e : Exception){
+            call.respond(HttpStatusCode.BadRequest, Simple_Response(false,"hii  qty${e.message}"))
+            return@post
         }
+
+        try{
+            db.addtocart(email!!, product as Product,qty!!)
+            call.respond(HttpStatusCode.OK,Simple_Response(true,"Cart Added SuccesFully"))
+        }catch (e : Exception){
+            call.respond(HttpStatusCode.NotAcceptable,Simple_Response(false,e.message.toString()))
+        }
+
     }
 
 
