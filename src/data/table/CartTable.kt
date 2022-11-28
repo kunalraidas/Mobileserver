95c017186e52498eed00500b8abe28d1ef6c5564
kunal <Kunalvirendra2510@outlook.com>
2022-11-28 14:10:34 +0530

    cart change

--- src/data/table/CartTable.kt
+++ src/data/table/CartTable.kt
@@ -4,10 +4,11 @@ import org.jetbrains.exposed.sql.Table
 
 object CartTable : Table()
 {
-    val Cart_id = integer("Cart_id")
+    val Cart_id = integer("Cart_id").autoIncrement()
     val Email = reference("Cust_id",CustomerTable.Email)
     val Product_id =  reference("Product_id",ProductTable.Product_id)
     val Quentity = integer("quentity")
+
     // Count Total Price base price of product * quentity
     val TotalPrice = float("totalPrice")
 //   val Mobile_id = reference("Mobile_id",MobileTable.Mobile_id)
--- src/repository/Cart_Repo.kt
+++ src/repository/Cart_Repo.kt
@@ -4,22 +4,43 @@ import com.example.data.model.Cart
 import com.example.data.table.CartTable
 import com.example.data.table.CustomerTable
 import com.example.data.table.ProductTable
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
-
-class Cart_Repo
+import org.jetbrains.exposed.sql.*
+import com.example.repository.Database_Factory.dbQuery
+open class Cart_Repo
 {
     suspend fun addCartItem(cart: Cart){
         Database_Factory.dbQuery {
             CartTable.insert { ct->
-                ct[CartTable.Cart_id] = cart.cart_id
+                // ct[CartTable.Cart_id] = cart.cart_id
                 ct[CartTable.Email] = CustomerTable.Email
                 ct[CartTable.Product_id] = ProductTable.Product_id
+
                 ct[CartTable.Quentity] = cart.quentity
                 ct[CartTable.TotalPrice] = cart.total_price
             }
         }
     }
+    suspend fun removeformCart(id: Int) = dbQuery{
+        CartTable.deleteWhere {
+            CartTable.Cart_id.eq(id)
+        }
+    }
+
+    suspend fun getCart(id: Int) = dbQuery{
+        CartTable.select {
+            CartTable.Cart_id.eq(id)
+        }
+    }
+
+    suspend fun UpdateQuentity(id: Int,qty : Int) = dbQuery {
+        CartTable.update(where = { CartTable.Cart_id.eq(id) }){ t ->
+            t[Quentity] = qty
+
+        }
+
+    }
+
+
 
     private fun rowToCart(row: ResultRow) : Cart?
     {
